422e42c05e718461ee4d50e81410d4d8e269e578
aldocenga <eraldocenga@nmc.al>
2023-09-22 09:23:58 +0200

    Meals Summary style added

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Fragment } from 'react';
 import HeaderIMG from '../../assets/food.jpg';
 import ButtonHeaderCart from './ButtonHeaderCart';
-
+import MealsSummary from '../Meals/MealsSummary';
 
 const Header = (props) => {
-
-return  <Fragment>
-            
-            <header class="w-full h-11 bg-slate-600 flex justify-between items-center px-11 text-white">
+    return (
+        <div className="relative">
+            <header className="w-full h-11 bg-slate-600 flex justify-between items-center px-11 text-white z-10">
                 <h1>YourMeals</h1>
-                <ButtonHeaderCart/>
+                <ButtonHeaderCart />
             </header>
-            <div>
-                <img class="w-full h-72 object-cover origin-top-left"  src={HeaderIMG}/>
+            <img className="w-full h-96 object-cover origin-top-left" src={HeaderIMG} alt="Header Food" />
+            <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-2/3 text-white w-1/2">
+                <MealsSummary />
             </div>
-            
-        </Fragment>
+        </div>
+    );
 }
 
 export default Header;
--- src/Components/Meals/MealsSummary.js
+++ src/Components/Meals/MealsSummary.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-function MealsSummary () {
-
+function MealsSummary() {
     return (
-        <section>
-            <h2>Today's Special Meals</h2>
-            <p>We are excited to present our special meals for the day, 
-                featuring a sumptuous roasted chicken served with a side of garlic mashed potatoes and seasonal vegetables. 
-                To end your meal, enjoy our chef's signature chocolate mousse with fresh berries. Bon appétit!
+        <section className="bg-gradient-to-tr from-yellow-400 via-red-500 to-pink-500 p-8 rounded-lg shadow-md text-white max-w-lg mx-auto mt-10">
+            <h2 className="text-3xl font-semibold mb-4">Today's Special Meals</h2>
+            <p className="text-lg">
+                We are excited to present our special meals for the day, featuring a sumptuous 
+                roasted chicken served with a side of garlic mashed potatoes and seasonal vegetables. 
             </p>
         </section>
-    )
-
-};
+    );
+}
 
 export default MealsSummary;
